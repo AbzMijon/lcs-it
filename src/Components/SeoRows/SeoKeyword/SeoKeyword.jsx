e155466fbc0654e4c5585498ee17b1c788a69d0a
AbzMijon <egoraksevic@gmail.com>
2023-04-04 17:49:44 +0300

    some change in keyword cards

--- src/Components/SeoRows/SeoKeyword/SeoKeyword.jsx
+++ src/Components/SeoRows/SeoKeyword/SeoKeyword.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ContactBtn from '../../ContactBtn/ContactBtn';
 import './seoKeyword.scss';
+import facebook from '../../../assets/svg/facebook.svg';
+import instagram from '../../../assets/svg/inst.svg';
+import linkedIn from '../../../assets/svg/linkedIn.svg';
+import telegram from '../../../assets/svg/telegram.svg';
 
 function SeoKeyword({ setModal }) {
     return (
@@ -47,13 +51,41 @@ function SeoKeyword({ setModal }) {
             </li>
 
             <li className='seo__card'>
-                <p className='seo__card-text'>You have a decent chance of ranking for</p>
+                <div className='seo__card-head'>
+                    <div className='seo__card-head-title'>
+                        <span className='seo__card-head-circle seo__card-head-circle--purple'></span> 
+                        <p>Product 1</p>
+                    </div>
+                    <div className='seo__card-head-title'> 
+                        <span className='seo__card-head-circle seo__card-head-circle--pink'></span> 
+                        <p>Product 2</p>
+                    </div>
+                </div>
+                <p className='seo__card-text'>Strategy Development</p>
                 <div className='seo-btn'>
                     <ContactBtn setModal={setModal}/>
                 </div>
             </li>
             <li className='seo__card'>
-                <p className='seo__card-text'>You have a decent chance of ranking for</p>
+                <div className='seo__card-head'>
+                    <div className='seo__card-head-title'>
+                        <ul className='footer__folow-list'>
+                            <li className='footer__folow-item'>
+                                <img src={facebook} alt="" className='footer__folow-img' />
+                            </li>
+                            <li className='footer__folow-item'>
+                                <img src={instagram} alt="" className='footer__folow-img' />
+                            </li>
+                            <li className='footer__folow-item'>
+                                <img src={linkedIn} alt="" className='footer__folow-img' />
+                            </li>
+                            <li className='footer__folow-item'>
+                                <img src={telegram} alt="" className='footer__folow-img' />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <p className='seo__card-text'>Creative Creation</p>
                 <div className='seo-btn'>
                     <ContactBtn setModal={setModal}/>
                 </div>
