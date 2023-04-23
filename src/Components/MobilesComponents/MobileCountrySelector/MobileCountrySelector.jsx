7a6dbe28df1597be055f3d7a5642d1d3ba21f9f9
AbzMijon <egorchiktop228@gmail.com>
2023-04-23 14:16:43 +0300

    add adaprtive changes

--- src/Components/MobilesComponents/MobileCountrySelector/MobileCountrySelector.jsx
+++ src/Components/MobilesComponents/MobileCountrySelector/MobileCountrySelector.jsx
@@ -23,22 +23,26 @@ function MobileCountrySelector() {
                 className={open ? "countrySelector__current countrySelector__current--active" : "countrySelector__current"} 
                 onClick={() => setOpen((prev) => !prev)}
             >
-                <img src={activeCountry.src} alt="" className='countrySelector__currentItem-img' />
-                <p className='countrySelector__currentItem-name'>{activeCountry.label}</p>
-                {open ?
-                    <MdKeyboardArrowUp className='countrySelector__arrow'/>
-                :
-                    <MdKeyboardArrowDown className='countrySelector__arrow'/>
-                }
+                <div className="countrySelector__current-container">
+                    <img src={activeCountry.src} alt="" className='countrySelector__currentItem-img' />
+                    <p className='countrySelector__currentItem-name'>{activeCountry.label}</p>
+                    {open ?
+                        <MdKeyboardArrowUp className='countrySelector__arrow'/>
+                    :
+                        <MdKeyboardArrowDown className='countrySelector__arrow'/>
+                    }
+                </div>
             </div>
             {open &&
                 <ul className='countrySelector__list'>
                     {notActiveCountries && notActiveCountries.map((notActiveCountry) => {
                         return (
                             <li className='countrySelector__item' key={notActiveCountry.id} onClick={() => selectCountry(notActiveCountry.id)}>
-                                <img src={notActiveCountry.src} alt="" className='countrySelector__item-img' />
-                                <p className='countrySelector__item-name'>{notActiveCountry.label}</p>
-                                <MdKeyboardArrowDown className='countrySelector__arrow' />
+                                <div className="countrySelector__item-container">
+                                    <img src={notActiveCountry.src} alt="" className='countrySelector__item-img' />
+                                    <p className='countrySelector__item-name'>{notActiveCountry.label}</p>
+                                    <MdKeyboardArrowDown className='countrySelector__arrow' />
+                                </div>
                             </li>
                         )
                     })}
--- src/Components/MobilesComponents/MobileSeoSelector/MobileSeoSelector.jsx
+++ src/Components/MobilesComponents/MobileSeoSelector/MobileSeoSelector.jsx
@@ -19,24 +19,31 @@ function MobileSeoSelector() {
 
     return (
         <div className='seoSelector'>
-            <div className="seoSelector__current" onClick={() => setOpen((prev) => !prev)}>
+            <div 
+                className={open ? "seoSelector__current seoSelector__current--active" : "seoSelector__current"} 
+                onClick={() => setOpen((prev) => !prev)}
+            >
+            <div className="seoSelector__currentItem-container">
                 <h2 className='seoSelector__title'>SEO</h2>
                 <div className='seoSelector__currentItem'>
-                    <p className='seoSelector__currentItem-name'>{activeTab.label}</p>
-                    {open ?
-                        <MdKeyboardArrowUp className='seoSelector__arrow'/>
-                    :
-                        <MdKeyboardArrowDown className='seoSelector__arrow'/>
-                    }
+                        <p className='seoSelector__currentItem-name'>{activeTab.label}</p>
+                        {open ?
+                            <MdKeyboardArrowUp className='seoSelector__arrow'/>
+                        :
+                            <MdKeyboardArrowDown className='seoSelector__arrow'/>
+                        }
                 </div>
+            </div>
             </div>
             {open &&
                 <ul className='seoSelector__list'>
                     {notActiveTabs && notActiveTabs?.map((notActiveTab) => {
                         return (
                             <li className='seoSelector__item' key={notActiveTab.id} onClick={() => selectTab(notActiveTab.id)}>
-                                <p className='seoSelector__item-name'>{notActiveTab.label}</p>
-                                <MdKeyboardArrowDown className='seoSelector__arrow' />
+                                <div className="seoSelector__item-container">
+                                    <p className='seoSelector__item-name'>{notActiveTab.label}</p>
+                                    <MdKeyboardArrowDown className='seoSelector__arrow' />
+                                </div>
                             </li>
                         )
                     })}
