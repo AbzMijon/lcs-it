1e191990736c1edf274d9af71c427ecfccb37f60
AbzMijon <egorwebdev8@gmail.com>
2023-06-07 16:17:53 +0300

    add headers to post

--- src/api/messageApi.js
+++ src/api/messageApi.js
@@ -1,3 +1,8 @@
 import { instance } from './instanse';
 
-export const sendMessage = (formValues) => instance.post('', formValues);
+export const sendMessage = (formValues) => instance.post('', formValues, {
+    maxBodyLength: Infinity,
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+});
