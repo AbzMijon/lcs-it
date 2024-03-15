9f063e47fec7be8e940d50f7680ba4cdb544c0f8
AbzMijon <egorwebdev8@gmail.com>
2024-03-15 15:02:41 +0300

    cube

--- src/Components/Cube/Cube.jsx
+++ src/Components/Cube/Cube.jsx
@@ -1,10 +1,11 @@
+import Spline from '@splinetool/react-spline';
 import './cube.scss';
 
 function Cube({ isCubeInView }) {
     return (
         <div className={isCubeInView ? "welcome__cube" : "welcome__cube welcome__cube--hidden"}>
             <div className="cube">
-                <canvas id="canvas3d"></canvas>
+                <Spline scene="https://prod.spline.design/D1U4yp3F1SqMb2-p/scene.splinecode" />
             </div>
         </div>
     )
