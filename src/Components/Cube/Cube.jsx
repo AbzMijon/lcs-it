6964fa6c787155f8c21c507647aaaeb8d634a730
AbzMijon <egorwebdev8@gmail.com>
2024-03-16 16:59:16 +0300

    qwe

--- src/Components/Cube/Cube.jsx
+++ src/Components/Cube/Cube.jsx
@@ -2,11 +2,14 @@ import Spline from '@splinetool/react-spline';
 import './cube.scss';
 
 function Cube({ isCubeInView }) {
-    return (
-        <div className={isCubeInView ? "welcome__cube" : "welcome__cube welcome__cube--hidden"}>
-            <Spline scene="https://prod.spline.design/H8nHCWNUx4ogLNkX/scene.splinecode" />
-        </div>
-    )
+    if(window.innerWidth > 650) {
+        return (
+            <div className={isCubeInView ? "welcome__cube" : "welcome__cube welcome__cube--hidden"}>
+                <Spline scene="https://prod.spline.design/H8nHCWNUx4ogLNkX/scene.splinecode" />
+            </div>
+        )
+    }
+    return null;
 }
 
 export default Cube;
