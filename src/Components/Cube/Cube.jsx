2ed3e848ec281d2cff1f4f59d76e44d2bbfbb85c
AbzMijon <egorwebdev8@gmail.com>
2024-03-17 16:44:06 +0300

    set fetchpriority to cube

--- src/Components/Cube/Cube.jsx
+++ src/Components/Cube/Cube.jsx
@@ -5,9 +5,9 @@ const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 function Cube({ isCubeInView }) {
     return (
-        <div className={isCubeInView ? "welcome__cube" : "welcome__cube welcome__cube--hidden"}>
-            <Suspense fallback={<></>}>
-                <Spline scene="https://prod.spline.design/H8nHCWNUx4ogLNkX/scene.splinecode" />
+        <div className={isCubeInView ? "welcome__cube" : "welcome__cube welcome__cube--hidden"} fetchpriority='high'>
+            <Suspense>
+                <Spline scene="https://prod.spline.design/aMm7nD09t3VjF49r/scene.splinecode" />
             </Suspense>
         </div>
     )
